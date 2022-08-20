f9b088137a5891dad295425f4b5f8c8f18c5494a
Brainer21 <jarisl.ultio.21@gmail.com>
2022-08-20 19:29:39 +0100

    =Upgraded routes importing

--- resources/js/src/App.js
+++ resources/js/src/App.js
@@ -17,66 +17,89 @@ import { authCheckState } from './store/actions/auth';
 
 import 'aos/dist/aos.css';
 
-// Manager routes
-const asyncManagerCmsGlobal = lazy(() => import('./containers/Backend/Manager/Cms/Global'));
-const asyncManagerCmsGeneral = lazy(() => import('./containers/Backend/Manager/Cms/General'));
-const asyncManagerCmsAuth = lazy(() => import('./containers/Backend/Manager/Cms/Auth'));
-const asyncManagerCmsBackend = lazy(() => import('./containers/Backend/Manager/Cms/Backend'));
-const asyncManagerCmsFrontend = lazy(() => import('./containers/Backend/Manager/Cms/Frontend'));
+const importedRoutes = {
+    // Backend routes
+    Backend: {
+        // Manager routes
+        Manager: {
+            Dashboard: lazy(() => import('./containers/Backend/Manager/Dashboard')),
+            Cms: {
+                Global: lazy(() => import('./containers/Backend/Manager/Cms/Global')),
+                General: lazy(() => import('./containers/Backend/Manager/Cms/General')),
+                Auth: lazy(() => import('./containers/Backend/Manager/Cms/Auth')),
+                Backend: lazy(() => import('./containers/Backend/Manager/Cms/Backend')),
+                Frontend: lazy(() => import('./containers/Backend/Manager/Cms/Frontend'))
+            },
+            Settings: {
+                Language: lazy(() => import('./containers/Backend/Manager/Settings/Language')),
+            },
+            Features: {
+                Index: lazy(() => import('./containers/Backend/Manager/Features')),
+                Add: lazy(() => import('./containers/Backend/Manager/Features/Add')),
+                Edit: lazy(() => import('./containers/Backend/Manager/Features/Edit'))
+            },
+            Languages: {
+                Index: lazy(() => import('./containers/Backend/Manager/Languages')),
+                Add: lazy(() => import('./containers/Backend/Manager/Languages/Add')),
+                Edit: lazy(() => import('./containers/Backend/Manager/Languages/Edit'))
+            },
+            Roles: {
+                Index: lazy(() => import('./containers/Backend/Manager/Roles')),
+                Add: lazy(() => import('./containers/Backend/Manager/Roles/Add')),
+                Edit: lazy(() => import('./containers/Backend/Manager/Roles/Edit'))
+            },
+            Experiences: {
+                Index: lazy(() => import('./containers/Backend/Manager/Experiences')),
+                Add: lazy(() => import('./containers/Backend/Manager/Experiences/Add')),
+                Edit: lazy(() => import('./containers/Backend/Manager/Experiences/Edit')),
+            },
+            Skills: {
+                Index: lazy(() => import('./containers/Backend/Manager/Skills')),
+                Add: lazy(() => import('./containers/Backend/Manager/Skills/Add')),
+                Edit: lazy(() => import('./containers/Backend/Manager/Skills/Edit')),
+            },
+            Projects: {
+                Index: lazy(() => import('./containers/Backend/Manager/Projects')),
+                Add: lazy(() => import('./containers/Backend/Manager/Projects/Add')),
+                Edit: lazy(() => import('./containers/Backend/Manager/Projects/Edit'))
+            },
+            Technologies: {
+                Index: lazy(() => import('./containers/Backend/Manager/Technologies')),
+                Add: lazy(() => import('./containers/Backend/Manager/Technologies/Add')),
+                Edit: lazy(() => import('./containers/Backend/Manager/Technologies/Edit')),
+            }
+        },
 
-const asyncManagerDashboard = lazy(() => import('./containers/Backend/Manager/Dashboard'));
+        // Admin routes
+        Admin: {
+            Admins: {
+                Index: lazy(() => import('./containers/Backend/Admin/Admins')),
+                Add: lazy(() => import('./containers/Backend/Admin/Admins/Add')),
+                Edit: lazy(() => import('./containers/Backend/Admin/Admins/Edit')),
+            }
+        }
+    },
 
-const asyncManagerFeatures = lazy(() => import('./containers/Backend/Manager/Features'));
-const asyncManagerFeaturesAdd = lazy(() => import('./containers/Backend/Manager/Features/Add'));
-const asyncManagerFeaturesEdit = lazy(() => import('./containers/Backend/Manager/Features/Edit'));
+    // Auth routes
+    Auth: {
+        User: {
+            Login: lazy(() => import('./containers/Auth/User/Login'))
+        },
+        Admin: {
+            Login: lazy(() => import('./containers/Auth/Admin/Login')),
+            Verify: lazy(() => import('./containers/Auth/Admin/Verify'))
+        }
+    },
 
-const asyncManagerLanguages = lazy(() => import('./containers/Backend/Manager/Languages'));
-const asyncManagerLanguagesAdd = lazy(() => import('./containers/Backend/Manager/Languages/Add'));
-const asyncManagerLanguagesEdit = lazy(() => import('./containers/Backend/Manager/Languages/Edit'));
-
-const asyncManagerRoles = lazy(() => import('./containers/Backend/Manager/Roles'));
-const asyncManagerRolesAdd = lazy(() => import('./containers/Backend/Manager/Roles/Add'));
-const asyncManagerRolesEdit = lazy(() => import('./containers/Backend/Manager/Roles/Edit'));
-
-const asyncManagerSettingsLanguage = lazy(() => import('./containers/Backend/Manager/Settings/Language'));
-
-const asyncManagerUsers = lazy(() => import('./containers/Backend/Manager/Users'));
-const asyncManagerUsersAdd = lazy(() => import('./containers/Backend/Manager/Users/Add'));
-const asyncManagerUsersEdit = lazy(() => import('./containers/Backend/Manager/Users/Edit'));
-
-const asyncManagerExperiences = lazy(() => import('./containers/Backend/Manager/Experiences'));
-const asyncManagerExperiencesAdd = lazy(() => import('./containers/Backend/Manager/Experiences/Add'));
-const asyncManagerExperiencesEdit = lazy(() => import('./containers/Backend/Manager/Experiences/Edit'));
-
-const asyncManagerSkills = lazy(() => import('./containers/Backend/Manager/Skills'));
-const asyncManagerSkillsAdd = lazy(() => import('./containers/Backend/Manager/Skills/Add'));
-const asyncManagerSkillsEdit = lazy(() => import('./containers/Backend/Manager/Skills/Edit'));
-
-const asyncManagerProjects = lazy(() => import('./containers/Backend/Manager/Projects'));
-const asyncManagerProjectsAdd = lazy(() => import('./containers/Backend/Manager/Projects/Add'));
-const asyncManagerProjectsEdit = lazy(() => import('./containers/Backend/Manager/Projects/Edit'));
-
-const asyncManagerTechnologies = lazy(() => import('./containers/Backend/Manager/Technologies'));
-const asyncManagerTechnologiesAdd = lazy(() => import('./containers/Backend/Manager/Technologies/Add'));
-const asyncManagerTechnologiesEdit = lazy(() => import('./containers/Backend/Manager/Technologies/Edit'));
-
-// Admin routes
-const asyncAdminAdmins = lazy(() => import('./containers/Backend/Admin/Admins'));
-const asyncAdminAdminsAdd = lazy(() => import('./containers/Backend/Admin/Admins/Add'));
-const asyncAdminAdminsEdit = lazy(() => import('./containers/Backend/Admin/Admins/Edit'));
-
-// Auth routes
-const asyncAuthUserLogin = lazy(() => import('./containers/Auth/User/Login'));
-
-const asyncAuthAdminLogin = lazy(() => import('./containers/Auth/Admin/Login'));
-const asyncAuthAdminVerify = lazy(() => import('./containers/Auth/Admin/Verify'));
-
-// Frontend routes
-const asyncHome = lazy(() => import('./containers/Frontend/Home'));
-const asyncAbout = lazy(() => import('./containers/Frontend/About'));
-const asyncSkills = lazy(() => import('./containers/Frontend/Skills'));
-const asyncPortfolio = lazy(() => import('./containers/Frontend/Portfolio'));
-const asyncContact = lazy(() => import('./containers/Frontend/Contact'));
+    // Frontend routes
+    Frontend: {
+        Home: lazy(() => import('./containers/Frontend/Home')),
+        About: lazy(() => import('./containers/Frontend/About')),
+        Contact: lazy(() => import('./containers/Frontend/Contact')),
+        Skills: lazy(() => import('./containers/Frontend/Skills')),
+        Portfolio: lazy(() => import('./containers/Frontend/Portfolio'))
+    }
+};
 
 class App extends Component {
     componentDidMount() {
@@ -95,11 +118,11 @@ class App extends Component {
         const frontendRoutes = <Route path="/">
             <FrontendLayout>
                 <Switch>
-                    <Route path="/contact" component={asyncContact} />
-                    <Route path="/portfolio" component={asyncPortfolio} />
-                    <Route path="/skills" component={asyncSkills} />
-                    <Route path="/about" component={asyncAbout} />
-                    <Route path="/" component={asyncHome} />
+                    <Route path="/contact" component={importedRoutes.Frontend.Contact} />
+                    <Route path="/portfolio" component={importedRoutes.Frontend.Portfolio.Index} />
+                    <Route path="/skills" component={importedRoutes.Frontend.Skills.Index} />
+                    <Route path="/about" component={importedRoutes.Frontend.About} />
+                    <Route path="/" component={importedRoutes.Frontend.Home} />
                 </Switch>
             </FrontendLayout>
         </Route>;
@@ -108,8 +131,8 @@ class App extends Component {
             <Route path="/auth/admin">
                 <AuthAdminLayout>
                     <Switch>
-                        <Route path="/auth/admin/verify" component={asyncAuthAdminVerify} />
-                        <Route path="/auth/admin/login" component={asyncAuthAdminLogin} />
+                        <Route path="/auth/admin/verify" component={importedRoutes.Auth.Admin.Verify} />
+                        <Route path="/auth/admin/login" component={importedRoutes.Auth.Admin.Login} />
                     </Switch>
                 </AuthAdminLayout>
             </Route>
@@ -118,7 +141,7 @@ class App extends Component {
             <Route path="/auth/user">
                 <AuthUserLayout>
                     <Switch>
-                        <Route path="/auth/user/login" component={asyncAuthUserLogin} />
+                        <Route path="/auth/user/login" component={importedRoutes.Auth.User.Login} />
                     </Switch>
                 </AuthUserLayout>
             </Route>
@@ -132,53 +155,53 @@ class App extends Component {
                 <Route path={['/user', '/admin']}>
                     <BackendManagerLayout>
                         <Switch>
-                            <Route path="/admin/admins/:id/edit" component={asyncAdminAdminsEdit} />
-                            <Route path="/admin/admins/add" component={asyncAdminAdminsAdd} />
-                            <Route path="/admin/admins" component={asyncAdminAdmins} />
+                            <Route path="/admin/admins/:id/edit" component={importedRoutes.Backend.Admin.Admins.Edit} />
+                            <Route path="/admin/admins/add" component={importedRoutes.Backend.Admin.Admins.Add} />
+                            <Route path="/admin/admins" component={importedRoutes.Backend.Admin.Admins.Index} />
 
-                            <Route path="/:manager/cms/global" component={asyncManagerCmsGlobal} />
-                            <Route path="/:manager/cms/general" component={asyncManagerCmsGeneral} />
-                            <Route path="/:manager/cms/auth" component={asyncManagerCmsAuth} />
-                            <Route path="/:manager/cms/backend" component={asyncManagerCmsBackend} />
-                            <Route path="/:manager/cms/frontend" component={asyncManagerCmsFrontend} />
+                            <Route path="/:manager/cms/global" component={importedRoutes.Backend.Manager.Cms.Global} />
+                            <Route path="/:manager/cms/general" component={importedRoutes.Backend.Manager.Cms.General} />
+                            <Route path="/:manager/cms/auth" component={importedRoutes.Backend.Manager.Cms.Auth} />
+                            <Route path="/:manager/cms/backend" component={importedRoutes.Backend.Manager.Cms.Backend} />
+                            <Route path="/:manager/cms/frontend" component={importedRoutes.Backend.Manager.Cms.Frontend} />
 
-                            <Route path="/:manager/dashboard" component={asyncManagerDashboard} />
+                            <Route path="/:manager/dashboard" component={importedRoutes.Backend.Manager.Dashboard} />
 
-                            <Route path="/:manager/features/:id/edit" component={asyncManagerFeaturesEdit} />
-                            <Route path="/:manager/features/add" component={asyncManagerFeaturesAdd} />
-                            <Route path="/:manager/features" component={asyncManagerFeatures} />
+                            <Route path="/:manager/features/:id/edit" component={importedRoutes.Backend.Manager.Features.Edit} />
+                            <Route path="/:manager/features/add" component={importedRoutes.Backend.Manager.Features.Add} />
+                            <Route path="/:manager/features" component={importedRoutes.Backend.Manager.Features.Index} />
 
-                            <Route path="/:manager/languages/:id/edit" component={asyncManagerLanguagesEdit} />
-                            <Route path="/:manager/languages/add" component={asyncManagerLanguagesAdd} />
-                            <Route path="/:manager/languages" component={asyncManagerLanguages} />
+                            <Route path="/:manager/languages/:id/edit" component={importedRoutes.Backend.Manager.Languages.Edit} />
+                            <Route path="/:manager/languages/add" component={importedRoutes.Backend.Manager.Languages.Add} />
+                            <Route path="/:manager/languages" component={importedRoutes.Backend.Manager.Languages.Index} />
 
-                            <Route path="/:manager/roles/:id/edit" component={asyncManagerRolesEdit} />
-                            <Route path="/:manager/roles/add" component={asyncManagerRolesAdd} />
-                            <Route path="/:manager/roles" component={asyncManagerRoles} />
+                            <Route path="/:manager/roles/:id/edit" component={importedRoutes.Backend.Manager.Roles.Edit} />
+                            <Route path="/:manager/roles/add" component={importedRoutes.Backend.Manager.Roles.Add} />
+                            <Route path="/:manager/roles" component={importedRoutes.Backend.Manager.Roles.Index} />
 
-                            <Route path="/:manager/settings/language" component={asyncManagerSettingsLanguage} />
+                            <Route path="/:manager/settings/language" component={importedRoutes.Backend.Manager.Settings.Language} />
 
-                            <Route path="/:manager/users/:id/edit" component={asyncManagerUsersEdit} />
-                            <Route path="/:manager/users/add" component={asyncManagerUsersAdd} />
-                            <Route path="/:manager/users" component={asyncManagerUsers} />
+                            <Route path="/:manager/users/:id/edit" component={importedRoutes.Backend.Manager.Users.Edit} />
+                            <Route path="/:manager/users/add" component={importedRoutes.Backend.Manager.Users.Add} />
+                            <Route path="/:manager/users" component={importedRoutes.Backend.Manager.Users.Index} />
 
 
 
-                            <Route path="/:manager/experiences/:id/edit" component={asyncManagerExperiencesEdit} />
-                            <Route path="/:manager/experiences/add" component={asyncManagerExperiencesAdd} />
-                            <Route path="/:manager/experiences" component={asyncManagerExperiences} />
+                            <Route path="/:manager/experiences/:id/edit" component={importedRoutes.Backend.Manager.Experiences.Edit} />
+                            <Route path="/:manager/experiences/add" component={importedRoutes.Backend.Manager.Experiences.Add} />
+                            <Route path="/:manager/experiences" component={importedRoutes.Backend.Manager.Experiences.Index} />
 
-                            <Route path="/:manager/skills/:id/edit" component={asyncManagerSkillsEdit} />
-                            <Route path="/:manager/skills/add" component={asyncManagerSkillsAdd} />
-                            <Route path="/:manager/skills" component={asyncManagerSkills} />
+                            <Route path="/:manager/skills/:id/edit" component={importedRoutes.Backend.Manager.Skills.Edit} />
+                            <Route path="/:manager/skills/add" component={importedRoutes.Backend.Manager.Skills.Add} />
+                            <Route path="/:manager/skills" component={importedRoutes.Backend.Manager.Skills.Index} />
 
-                            <Route path="/:manager/projects/:id/edit" component={asyncManagerProjectsEdit} />
-                            <Route path="/:manager/projects/add" component={asyncManagerProjectsAdd} />
-                            <Route path="/:manager/projects" component={asyncManagerProjects} />
+                            <Route path="/:manager/projects/:id/edit" component={importedRoutes.Backend.Manager.Projects.Edit} />
+                            <Route path="/:manager/projects/add" component={importedRoutes.Backend.Manager.Projects.Add} />
+                            <Route path="/:manager/projects" component={importedRoutes.Backend.Manager.Projects.Index} />
 
-                            <Route path="/:manager/technologies/:id/edit" component={asyncManagerTechnologiesEdit} />
-                            <Route path="/:manager/technologies/add" component={asyncManagerTechnologiesAdd} />
-                            <Route path="/:manager/technologies" component={asyncManagerTechnologies} />
+                            <Route path="/:manager/technologies/:id/edit" component={importedRoutes.Backend.Manager.Technologies.Edit} />
+                            <Route path="/:manager/technologies/add" component={importedRoutes.Backend.Manager.Technologies.Add} />
+                            <Route path="/:manager/technologies" component={importedRoutes.Backend.Manager.Technologies.Index} />
 
 
 
